9d3697f8caf19e43db71bd5dbfd12ec43151131d
William Rocha <uill_pf@hotmail.com>
2023-02-10 17:28:38 -0300

    First Commit

---
+++ Program.cs
@@ -0,0 +1,12 @@
+﻿// See https://aka.ms/new-console-template for more information
+
+Console.WriteLine("Os 20 primeiros números FIBONACCI são:");
+  int a = 0, b = 1, c = 0;  
+    Console.WriteLine("{0} {1}", a,b);  
+    for (int i = 2; i <= 20; i++)  
+    {  
+        c= a + b;  
+        Console.WriteLine("{0}", c);  
+        a= b;  
+        b= c;  
+    }
